7f5d9a57ca72d08f0712288b2a924c512c047d0a
Jason Chandler <Jasonflame3@gmail.com>
2023-09-16 17:11:30 -0600

    I finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /* simple program to make a spy name */
+        Console.Write("What is your first name? ");
+        string jcFirstName = Console.ReadLine();
+        Console.Write("What is YOur last name? ");
+        string jcLastName = Console.ReadLine();
+        
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {jcLastName}, {jcFirstName} {jcLastName}.");
     }
 }
