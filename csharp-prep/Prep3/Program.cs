7afa3cc2e0686b8a33f886fc49704c7a23325a47
Jason Chandler <Jasonflame3@gmail.com>
2023-09-22 16:49:47 -0600

    added random number generation

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,16 +1,20 @@
 using System;
+using System.Text.Json;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number?");
-        string jcMagicNumStr = Console.ReadLine();
-        int jcMagicNumInt = int.Parse(jcMagicNumStr);
+        // Console.Write("What is the magic number?");
+        // string jcMagicNumStr = Console.ReadLine();
+        // int jcMagicNumInt = int.Parse(jcMagicNumStr);
+
+        Random randomGenerator = new Random();
+        int jcMagicNumInt = randomGenerator.Next(1,100);
 
         bool jcFound = false;
         do         {
-            Console.Write("what is your first guess?");
+            Console.Write("what is your first guess? ");
             string jcGuessStr = Console.ReadLine();
             int jcGuessInt = int.Parse(jcGuessStr);
             if (jcGuessInt < jcMagicNumInt)
