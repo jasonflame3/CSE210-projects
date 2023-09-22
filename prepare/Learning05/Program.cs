085b254e817c6c176382b05392c516b6d4c7fbc3
Jason Chandler <Jasonflame3@gmail.com>
2023-09-22 12:50:20 -0600

    debugging

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // why is this the one being debugged?
+        Console.WriteLine("Hello Learning world05");
     }
 }
