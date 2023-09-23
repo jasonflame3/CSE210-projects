0749254d9a3a1d4a6fd9adfd75aabd429c10585d
Jason Chandler <Jasonflame3@gmail.com>
2023-09-22 19:03:15 -0600

    added functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Display the message, "welcome to the program"
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+        //Asks for and returns the user's name as as string
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string jcUserName = Console.ReadLine(); 
+            return jcUserName; 
+        }
+        
+        //asks for a returns the user's favorite number as an integer
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string jcFavStr = Console.ReadLine();
+            int jcFavNum = int.Parse(jcFavStr); 
+            return jcFavNum; 
+        }
+
+        //Accepts an integer as a parameter and returns that number squared as an integer
+        static int SquareNumber(int jcNum)
+        {
+            int jcSquare = jcNum *jcNum;
+            return jcSquare;
+        }
+
+        //Accepts the user's name and the squared number and displays them.
+        static void DisplayResult(string jcUserName, int jcSquare)
+        {
+            Console.WriteLine($"{jcUserName}, the square of you number is {jcSquare}");
+        }
+
+        DisplayWelcome();
+        string jcUserName = PromptUserName();
+        int jcUserNumber = PromptUserNumber();
+        DisplayResult(jcUserName, SquareNumber(jcUserNumber));
     }
 }
