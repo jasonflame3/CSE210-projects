a7d5235b5bf06049f1d6400b8f8372dfc64aa753
Jason Chandler <Jasonflame3@gmail.com>
2023-09-25 21:57:52 -0600

    fixed syntax to add to list

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -22,8 +22,8 @@ class Program
         job2.DisplayJobDetails();
 
         Resume resume1 = new Resume();
-        resume1._jobs[0] = job1;
-        resume1._jobs[1] = job2;
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
 
         Console.WriteLine(resume1._jobs[0]._jobTitle);
             }
