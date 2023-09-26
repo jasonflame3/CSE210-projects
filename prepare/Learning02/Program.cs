be4d1a080ff48da84d149e55e635313308b9bd68
Jason Chandler <Jasonflame3@gmail.com>
2023-09-25 21:44:40 -0600

    added display meathod

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-
         Job job1 = new Job();
         job1._company = "Apple";
         job1._jobTitle = "Software Engineer";
