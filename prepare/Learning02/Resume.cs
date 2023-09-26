7ee9c37ceb97512c661f6520f8499c7d8ec0db48
Jason Chandler <Jasonflame3@gmail.com>
2023-09-25 22:04:29 -0600

    added display to resume

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -5,4 +5,14 @@ public class Resume
     }
     public string _name;
     public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job jobs in _jobs)
+        {
+            jobs.DisplayJobDetails();
+        }
+    }
 }
