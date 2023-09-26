82c5bf55ef4391e657d06cbae200e5f5fff027ae
Jason Chandler <Jasonflame3@gmail.com>
2023-09-25 21:33:51 -0600

    added job1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
             }
 }
