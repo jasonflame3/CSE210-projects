7947a51517ef72619bba70e02cc88a8130437d19
Jason Chandler <Jasonflame3@gmail.com>
2023-10-02 12:31:32 -0600

    progress made during class hour, have two classes started

---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,18 @@
+/*
+Name: <journal program>
+Purpose: < an application to help users write a journal
+by removing excuses not to do it by giving the user a prompt.>
+Author: Jason Chandler
+Date: (c) 2023
+*/
+
+public class Entry
+{
+    public Entry()
+    {}
+
+    public string _jcWords;
+    public string _jcPrompt;
+    public int _jcDate;
+
+}
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -6,7 +6,43 @@ public class Journal
 {
     public Journal()
     {}
-
     
+    public List<Entry> _jcEntries = new List<Entry>();
+    public List<string> _jcPrompts = new List<string>();
+    public string _jcFilename;
 
+    public void Display()
+    {
+
+    }
+
+    public void writeEntry()
+    {
+
+    }
+
+    public void save()
+    {
+
+    }
+
+    public void load()
+    {
+
+    }
+
+    public void DisplayMenu()
+    {
+        Console.WriteLine("Welcome to the journal program. What would you like to do?");
+        Console.WriteLine("Write a new entry.");
+        Console.WriteLine("Save journal to file");
+        Console.WriteLine("Load journal from file");
+        Console.WriteLine("Display Entries");
+        Console.WriteLine("Quit");
+    }
+
+    public void quit()
+    {
+
+    }
 }
