80c50b95672a180f8e2ac16f8987e0a5f35f2875
Jason Chandler <Jasonflame3@gmail.com>
2023-10-11 17:14:35 -0600

    finished the project

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction jcFraction1 = new Fraction();
+        
+        Fraction jcFraction2 = new Fraction(6);
+
+        Fraction jcFraction3 = new Fraction(6,7);
+        
+        jcFraction1.setFraction(3,7);
+        int jctop = jcFraction1.getTop();
+        int jcBottom = jcFraction1.getBottom();
+        Console.WriteLine($" {jctop} / {jcBottom}");
+        Console.WriteLine(jcFraction1.GetFractionString());
+        Console.WriteLine(jcFraction1.GetDecimalValue());
     }
 }
---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+using System.Dynamic;
+
+public class Fraction
+{
+    private int _jcTop;
+    private int _jcBottom;
+
+    public Fraction()
+    {
+        _jcTop = 1;
+        _jcBottom = 1;
+    }
+    public Fraction(int jcTop)
+    {
+        _jcTop = jcTop;
+    }
+    public Fraction(int jcTop, int jcBottom)
+    {
+        _jcTop = jcTop;
+        _jcBottom = jcBottom;
+    }
+
+    public void setFraction(int jcTop = 1, int jcBottom = 0)
+    {
+        _jcTop = jcTop;
+        _jcBottom = jcBottom;
+    }
+
+    public int getTop()
+    {
+        return _jcTop;
+    }
+
+    public int getBottom()
+    {
+        return _jcBottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_jcTop} / {_jcBottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)(_jcTop) / (double)(_jcBottom);
+    }
+}
