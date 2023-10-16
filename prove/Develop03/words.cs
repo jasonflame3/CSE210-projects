2ddd79d14c5f5e5d8169d2677fb3b751c62c581b
Newbeprog <113536782+Newbeprog@users.noreply.github.com>
2023-10-16 12:33:02 -0600

    updated words

--- prove/Develop03/words.cs
+++ prove/Develop03/words.cs
@@ -7,9 +7,26 @@ Purpose: To hide words within the scriptures until finally the whole scripture i
 Name: Kamas Peterson 
 (c) 2023
 */
-public class Word
+public class kpWord
 {
     private string _kpWord; 
     private bool _kpHidden; 
 
+    public kpHidewords(self)
+    {
+
+    }
+    
+    public kpWord(_kpWord, _kpHidden)
+    {
+
+
+    }
+
+    public kpDisplay()
+    {
+
+    }
+
 }
+
