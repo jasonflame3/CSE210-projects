38dbc2a6d24ab57436cdfc6ff5d7d718139164dd
Newbeprog <113536782+Newbeprog@users.noreply.github.com>
2023-10-16 12:10:49 -0600

    start words

---
+++ prove/Develop03/words.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel;
+
+/*
+W06 Prove: Developer 
+Purpose: To hide words within the scriptures until finally the whole scripture is gone. 
+Name: Kamas Peterson 
+(c) 2023
+*/
+public class Word
+{
+    private string _kpWord; 
+    private bool _kpHidden; 
+
+}
