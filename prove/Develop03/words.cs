e6408bb93e8f38f011dc1d69ec4bbe90307f4f71
Newbeprog <113536782+Newbeprog@users.noreply.github.com>
2023-10-18 11:53:48 -0600

    new code

--- prove/Develop03/words.cs
+++ prove/Develop03/words.cs
@@ -1,31 +1,42 @@
 using System;
 using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 /*
 W06 Prove: Developer 
 Purpose: To hide words within the scriptures until finally the whole scripture is gone. 
 Name: Kamas Peterson 
 (c) 2023
+Refrences:
+https://www.w3schools.com/cs/cs_for_loop.php
 */
 public class kpWord
 {
     private string _kpWord; 
     private bool _kpHidden; 
 
-    public kpHidewords()
+    public void kpHidewords()
     {
+       int kpLength = _kpWord.Length;
+       _kpWord = ""; 
+       for (int i = 0; i<= kpLength; i++)
+       {
+        _kpwords += "_";
+       }
+       
 
     }
     
-    public kpWord(_kpWord, _kpHidden)
+    public kpWord(string kpWord, bool kpHidden)
     {
-
-
+        _kpWord = kpWord; 
+        _kpHidden = kpHidden;
     }
 
-    public kpDisplay()
+    public void kpDisplay()
     {
-
+        Console.WriteLine($"{}");
+        Console.ReadLine(); 
     }
 
 }
