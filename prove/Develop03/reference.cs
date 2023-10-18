7b725693575e169bc6f8bb18abdf75854da10b93
lhurdman <122401409+lhurdman@users.noreply.github.com>
2023-10-18 10:45:04 -0600

    Update reference.cs

--- prove/Develop03/reference.cs
+++ prove/Develop03/reference.cs
@@ -1,7 +1,25 @@
 using System;
 
+/*
+W06 Prove: Developer 
+Purpose: Stores the book, chapter, and verse.
+Name: Lindsay Hurdman
+(c) 2023
+*/
 
-public class Reference
+class lhReference
 {
+    private string _book;
+    private string _chapter;
+    private string _verse;
 
+    public lhReference(_book, _chapter, _verse)
+    {
+        lhReference = _book, _chapter, _verse;
+    }
+
+    public lhReference(lhReference)
+    {
+        string lhReference;
+    }
 }
