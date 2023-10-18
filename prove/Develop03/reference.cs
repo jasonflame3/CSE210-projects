dd8627390745f092ebe6e8f4237a21f98a8a8f25
lhurdman <hur21012@byui.edu>
2023-10-18 11:53:57 -0600

    fixed sintax

--- prove/Develop03/reference.cs
+++ prove/Develop03/reference.cs
@@ -4,21 +4,25 @@ using System;
 W06 Prove: Developer 
 Purpose: Stores the book, chapter, and verse.
 Name: Lindsay Hurdman
+Sources: Chat GPT
 (c) 2023
 */
 
 class lhReference
 {
+    // Create atributes.
     private string _lhbook;
     private string _lhchapter;
     private string _lhverse;
 
-    public lhReference(string _lhbook, string _lhchapter, string _lhverse)
+    public lhReference(string lhbook, string lhchapter, string lhverse)
     {
-        lhReference = _lhbook, _lhchapter, _lhverse;
+        _lhbook = lhbook;
+        _lhchapter = lhchapter;
+        _lhverse = lhverse;
     }
 
-    public lhReference(lhReference)
+    public lhReference(string lhReference)
     {
         string lhReference;
     }
