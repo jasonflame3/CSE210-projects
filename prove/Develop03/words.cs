1fb4fccac5d7d8aeb30c6ea76bdedba7d1220ad0
Jason Chandler <Jasonflame3@gmail.com>
2023-10-18 11:53:48 -0600

    added source

--- prove/Develop03/words.cs
+++ prove/Develop03/words.cs
@@ -6,6 +6,9 @@ W06 Prove: Developer
 Purpose: To hide words within the scriptures until finally the whole scripture is gone. 
 Name: Kamas Peterson 
 (c) 2023
+
+
+https://www.tutorialspoint.com/How-to-calculate-the-length-of-the-string-using-Chash#:~:text=Use%20the%20String.,the%20length%20of%20the%20string.
 */
 public class kpWord
 {
