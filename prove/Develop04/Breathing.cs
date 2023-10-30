224e583e743e6763be708959ea55128fb544e924
reader124 <sereading9999@gmail.com>
2023-10-30 12:00:19 -0600

    made space for the methods of Breathing

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -3,4 +3,28 @@ using System;
 class Breathing : Activity
 {
 
+    //Constructors
+    public Breathing(string srName, string srDescription, float srTime) : base(srName, srDescription, srTime)
+    {
+
+    }
+
+    private string srDisplayMessage(bool srInhaling)
+    {
+
+
+
+
+        return "";
+    }
+
+    public void srDoBreathing()
+    {
+
+
+
+
+        
+    }
+
 }
