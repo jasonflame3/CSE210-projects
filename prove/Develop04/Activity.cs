bb062bec8a21ad0dacf708bcba556607254ade4a
reader124 <sereading9999@gmail.com>
2023-10-30 12:22:04 -0600

    finished the animation method

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -17,9 +17,20 @@ public Activity(string jcName, string jcDecription, float jcTime)
 }
 
 protected void jcAnimation(){
+    int srTimes = 3; //Change this to increase or decrease the duration of the animation
+    string srBack = "\b \b"; //the backspace character
 
+    Console.WriteLine(""); //create a space for the animation to happen in
 
+    for (int x = 1; x <= srTimes; x++) //do the animation
+    {
+    Console.Write(srBack + "-");
+    Console.Write(srBack + "\\");
+    Console.Write(srBack + "|");
+    Console.Write(srBack + "/");
+    }
 
+    Console.WriteLine(""); //end the space for the animation
 }
 
 protected void jcDisplayStartMessage(){
