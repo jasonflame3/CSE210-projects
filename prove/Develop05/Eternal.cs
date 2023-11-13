6f04f8a9929543c322a1564d3a26f89fcf45d527
reader124 <sereading9999@gmail.com>
2023-11-13 11:50:48 -0700

    started methods for eternal

--- prove/Develop05/Eternal.cs
+++ prove/Develop05/Eternal.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 /*
 W010 Prove: Developer 
@@ -7,9 +9,27 @@ Sean Reading
 11/13/2023
 */
 
-public class srEternal : Goal
+class srEternal : Goal
 {
     //constructors
     public srEternal() : base() {}
     public srEternal(string name, string description, int points) : base(name, "eternal", description, points, false) {}
+
+    //I don't know what this is supposed to do here but it's in the plan, so it's here too
+    public override void finished()
+    {
+
+    }
+
+    //return the number of points this goal is worth
+    public override int returnPoints()
+    {
+        return _kpPoints;
+    }
+
+    //return a string containing the name and description of the goal
+    public override string displayGoal()
+    {
+        return "[ ]" + _kpName + "(" + _kpDescription + ")";
+    }
 }
