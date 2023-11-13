2b837459eb6db068fc1da794405c64441611a79a
Newbeprog <113536782+Newbeprog@users.noreply.github.com>
2023-11-13 11:59:10 -0700

    added get

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,10 +1,56 @@
-using System; 
+using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 
-class Goal 
+/*
+W10 Prove: Developer
+
+*/
+
+abstract class Goal 
 {
     private string _kpName; 
     private string _kptypeGoal; 
     private string _kpDescription; 
     private int _kpPoints; 
     private bool _kpFinished; 
+
+    protected Goal (string kpName, string kptypeGoal, string kpDescription, int kpPoints, bool kpFinished)
+    {
+        _kpName = kpName;
+        _kptypeGoal = kptypeGoal;
+        _kpDescription = kpDescription; 
+        _kpPoints = kpPoints;
+        _kpFinished = kpFinished; 
+    }
+
+    protected abstract void Finished ();
+    
+
+    protected abstract void DisplayGoal ();
+   
+
+    protected abstract void ReturnPoints ();
+
+    protected string kpGetName ()
+    {
+        return _kpName;
+    }
+    protected string kpGettypeGoal ()
+    {
+        return _kptypeGoal;
+    }
+    protected string kpGetDescription ()
+    {
+        return _kpDescription;
+    }
+    protected int kpGetPoints ()
+    {
+        return _kpPoints;
+    }
+    protected bool kpGetFinished ()
+    {
+        return _kpFinished;
+    }
+    
 }
