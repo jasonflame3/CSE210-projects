b5a69703705a3aae0bfcf891d2e1885660ec2a25
reader124 <sereading9999@gmail.com>
2023-11-13 12:03:08 -0700

    syncing with Goal

--- prove/Develop05/Eternal.cs
+++ prove/Develop05/Eternal.cs
@@ -16,19 +16,19 @@ class srEternal : Goal
     public srEternal(string name, string description, int points) : base(name, "eternal", description, points, false) {}
 
     //I don't know what this is supposed to do for this class but it's in the plan, so it's here too
-    public override void finished()
+    public override bool Finished()
     {
-
+        return false;
     }
 
     //return the number of points this goal is worth
-    public override int returnPoints()
+    public override int ReturnPoints()
     {
         return _kpPoints;
     }
 
     //return a string containing the name and description of the goal
-    public override string displayGoal()
+    public override string DisplayGoal()
     {
         return "[ ]" + _kpName + " (" + _kpDescription + ")";
     }
