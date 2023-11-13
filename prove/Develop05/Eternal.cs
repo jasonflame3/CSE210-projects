9310a0b3196d8fb5af439f63865e8fa1dabd9a64
reader124 <sereading9999@gmail.com>
2023-11-13 11:37:41 -0700

    made eternal goal constructors

---
+++ prove/Develop05/Eternal.cs
@@ -0,0 +1,15 @@
+using System;
+
+/*
+W010 Prove: Developer 
+Eternal Goal
+Sean Reading
+11/13/2023
+*/
+
+public class srEternal : Goal
+{
+    //constructors
+    public srEternal() : base() {}
+    public srEternal(string name, string description, int points) : base(name, "eternal", description, points, false) {}
+}
