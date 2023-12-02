d2c1d2ec6cb5727f9553187aab543fe9f967aee8
Newbeprog <113536782+Newbeprog@users.noreply.github.com>
2023-12-02 13:43:51 -0700

    my stuff

---
+++ final/FinalProject/Puzzle.cs
@@ -0,0 +1,32 @@
+using System;
+/*
+W12 Prove: Developer 
+Description:
+Kamas Peterson 
+(c) 2023
+*/
+
+public class Puzzle : WheelofFortune 
+{
+    private List<string> _kpLetters = new List<string>();
+
+    public void kpWord ()
+    {
+
+    }
+
+    public void Display ()
+    {
+        
+    }
+    
+
+    public void kpRound ()
+    {
+        kpUser = true;
+
+        while (kpUser )
+        
+    }
+
+}
---
+++ final/FinalProject/Word.cs
@@ -0,0 +1,13 @@
+using System; 
+/*
+W12 Prove: Developer 
+Description: 
+Kamas Peterson 
+(c) 2023 
+*/
+
+public class Word : WheelofFortune
+{
+    private List<string> _kpWords = new List<string>();
+    
+}
